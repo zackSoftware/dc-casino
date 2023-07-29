8b5015bbe5313b5430aa4b2b968c9ce995d8e266
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-07-30 00:26:24 +0200

    feat(fxmanifest): use ox_lib and use_experimental_fxv2_oal

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,12 @@ description 'A casino resource for which you do not have to sell a kidney for'
 version '1.0.0'
 repository 'https://github.com/Disabled-Coding/dc-casino'
 
-shared_script 'config.lua'
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
 client_script 'client.lua'
 server_script 'server.lua'
 
 lua54 'yes'
+use_experimental_fxv2_oal 'yes'
